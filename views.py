f4be8a768cf9d927c10a7d3f6c2391f1c38cf9a2
abdull461 <abdullahabubakar461@gmail.com>
2023-05-01 09:21:09 +0100

    took some mistakenly added file from templates folder

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # blue print simply means all the routes are
 # inside it in this file
@@ -7,4 +7,4 @@ views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return "<h1>Hello world</h1>"
+    return render_template("home.html")
