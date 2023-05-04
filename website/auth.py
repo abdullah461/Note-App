7d79e4c7fa44105296ab713e9cddb30bea68d10a
abdull461 <abdullahabubakar461@gmail.com>
2023-05-04 06:47:07 +0100

    made some changes

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request, flash
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 # flash paackage is used when you want to flash some message on the screen
 
 # we use request package to get request of data from the form
@@ -21,19 +23,23 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 4 character', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 2 character', category='error')
         elif len(password1) < 7:
             flash('Password must be greater than 7 character', category='error')
         elif password1 != password2:
             flash('Password don\'t match.', category='error')
         else:
-            flash('Account created!', category='success')
             # add user to database
+            new_user = User(email=email, first_name=first_name, password = generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
