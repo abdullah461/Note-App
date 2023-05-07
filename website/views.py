d345c75e446684df3f7703b99df1c237647f8933
abdull461 <abdullahabubakar461@gmail.com>
2023-05-07 11:51:35 +0100

    adding note

--- website/views.py
+++ website/views.py
@@ -16,7 +16,7 @@ def home():
         if len(note) < 1:
             flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)
+            new_note = Note(note=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
