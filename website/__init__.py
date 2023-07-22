4e55bf5f9c89b27358df44eac05a1c84f1c7acd7
abdull461 <abdullahabubakar461@gmail.com>
2023-07-22 09:28:17 +0100

    added libraries

--- website/__init__.py
+++ website/__init__.py
@@ -3,17 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-# creating database
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    # the secret key secure the cookies and session data
+    
     app.config['SECRET_KEY'] = 'abdullahi'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     
-    # innitialize our database
+    
     db.init_app(app)
    
 
